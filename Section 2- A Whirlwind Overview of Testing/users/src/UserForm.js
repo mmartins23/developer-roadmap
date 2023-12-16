c61952a43035a8259b378fa03e69178bbcb56cf6
Marlon Martins <marlon-martins@outlook.com>
2023-12-16 21:06:48 +0100

    8. Adding the Form

---
+++ Section 2- A Whirlwind Overview of Testing/users/src/UserForm.js
@@ -0,0 +1,16 @@
+export default function UserForm() {
+  return (
+    <form>
+        <div>
+            <label>Name</label>
+            <input/>
+        </div>
+
+        <div>
+            <label>Email</label>
+            <input/>
+        </div>
+        <button>Add User</button>
+    </form>
+  )
+}
