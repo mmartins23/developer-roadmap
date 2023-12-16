fa6b8162d797fd9f659bf2e17e62271e98f45606
Marlon Martins <marlon-martins@outlook.com>
2023-12-16 23:35:49 +0100

    9. Handling User Input

--- Section 2- A Whirlwind Overview of Testing/users/src/UserForm.js
+++ Section 2- A Whirlwind Overview of Testing/users/src/UserForm.js
@@ -1,16 +1,24 @@
-export default function UserForm() {
-  return (
-    <form>
-        <div>
-            <label>Name</label>
-            <input/>
-        </div>
+import { useState } from 'react';
 
-        <div>
-            <label>Email</label>
-            <input/>
-        </div>
-        <button>Add User</button>
-    </form>
-  )
+export default function UserForm() {
+    const { name, setName } = useState("");
+    const { email, setEmail } = useState("");
+
+    const handleSubmit = (e) => {
+        e.prevent.default();
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name</label>
+                <input value={name} onChange={e => setName(e.target.value)} />
+            </div>
+
+            <div>
+                <label>Email</label>
+                <input value={email} onChange={e => setEmail(e.target.value)}/>
+            </div>
+            <button>Add User</button>
+        </form>
+    )
 }
